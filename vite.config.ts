dbf77f1946f2741d0896322cf9d1ec8ce0ca93f8
YuiryKho <hosroevurij@gmail.com>
2025-11-17 18:21:16 +0500

    Final build configuration fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ВАЖНО: Замените 'your-repo-name' на имя вашего репозитория на GitHub.
-  // Например, если URL вашего репозитория https://github.com/user/my-cool-app,
-  // то здесь должно быть base: '/my-cool-app/'
-  base: '/your-repo-name/'
+  // Указываем базовый путь для корректной работы на GitHub Pages.
+  base: '/tyumen-smart-city/'
 })
